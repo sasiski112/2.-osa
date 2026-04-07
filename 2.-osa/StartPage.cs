e4bc28b6ecafc9d5950697449c96180ec3631d6b
sasiski112 <mshevelovv@gmail.com>
2026-04-07 14:44:21 +0300

    Update StartPage.cs

--- 2.-osa/StartPage.cs
+++ 2.-osa/StartPage.cs
@@ -1,5 +1,7 @@
+using _3._osa;
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Net;
 
 namespace _2._osa
 {
@@ -7,8 +9,8 @@ namespace _2._osa
     {
         public static void Main()
         {
-           bool a = true;
-            while (a = true)
+            bool a = true;
+            while (a)
             {
                 Console.WriteLine("\n--- MENÜÜ ---");
                 Console.WriteLine("1 - Juku kinopilet");
@@ -69,15 +71,69 @@ namespace _2._osa
                     case "0":
                         Console.Clear();
                         Console.WriteLine("Head aega!");
-                        a = false; 
+                        a = false;
                         break;
 
                     default:
                         Console.Clear();
                         Console.WriteLine("Vale valik!");
-                        break;
+                        a = false;
+                        break;  
                 }
             }
         }
     }
 }
+//    internal class StartPage
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine("\n--- Menu ---");
+//                Console.WriteLine("1 - Ruudud");
+//                Console.WriteLine("2 - Käivita Analüüs");
+//                Console.WriteLine("3 - Inimesed");
+//                Console.WriteLine("4 - Märksõna");
+//                Console.WriteLine("5 - Arva Arv");
+//                Console.WriteLine("6 - Suurim Arv");
+//                Console.WriteLine("7 - Korrutustabel");
+//                Console.WriteLine("8 - Tudengid");
+//                Console.WriteLine("9 - ArvRud");
+//                Console.WriteLine("10 - PositivneNegativne");
+//                Console.WriteLine("11 - Keskmine");
+//                Console.WriteLine("12 - Kõige Suurem");
+//                Console.WriteLine("13 - Paari Loendused");
+//                Console.WriteLine("0 - Välju");
+//                Console.Write("Vali tegevus: ");
+
+//                string valik = Console.ReadLine();
+
+//                switch (valik)
+//                {
+//                    case "1": Funktsioon.Ruudud(); break;
+//                    case "2": Funktsioon.KäivitaAnalüüs(); break;
+//                    case "3": Funktsioon.Inimesed(); break;
+//                    case "4": Funktsioon.Märksõna(); break;
+//                    case "5": Funktsioon.ArvaArv(); break;
+//                    case "6": Funktsioon.SuurimArv(); break;
+//                    case "7": Funktsioon.Korrutustabel(); break;
+//                    case "8": Funktsioon.Tudengid(); break;
+//                    case "9": Funktsioon.ArvRud(); break;
+//                    case "10": Funktsioon.PositivneNegativne(); break;
+//                    case "11": Funktsioon.Keskmine(); break;
+//                    case "12": Funktsioon.KõigeSuurem(); break;
+//                    case "13": Funktsioon.PaariLoendused(); break;
+//                    case "0":
+//                        Console.WriteLine("Programmi lõpetamine...");
+//                        return;
+//                    default:
+//                        Console.WriteLine("Vale valik! Proovi uuesti.");
+//                        break;
+//                }
+
+//                Console.WriteLine("\n--- Tegevus lõpetatud ---\n");
+//            }
+//        }
+//    }
+//}
